06aafbfb628e83f5d750408a64df6c38b3bbf69a
Dawson <dawsonteu@gmail.com>
2020-03-17 20:55:43 -0400

    Added source files

---
+++ src/index.js
@@ -0,0 +1,42 @@
+/* TODO
+    Main Features
+        - Dashboard-style display
+            - Multiple sections each showing different information
+        - Multiple sections/panels
+            - Problems from followed users
+                - Add users to follow (Feature) (*)
+            - Problems with tagged/starred topics
+                - Star/Tag/Follow topics (Feature)
+            - Random problem/Problem of the day
+                - Skip/Find new problem (Feature)
+            - New/Hot Problems
+                - Change time period (Feature)
+            - Problem Search
+                - Filter by (Feature):
+                    - Point Value
+                    - Topic
+                    - etc.
+                - Show users that completed (Feature)
+            - Followed Users List
+                - (*) Location to add users to follow
+                - Popup/Foldout to see information - points, problems solved, etc. (Feature)
+        - Use DMOJ API to find information
+        - Problem View/Display for every problem
+            - Show Title, topic, point value, etc. (Feature)
+            - Popup/Foldout to see more detailed information
+                - Author
+                - Time/Memory Limit
+                - Problem Statement
+                - etc.
+    
+    Think about?
+        - Adding React to build UI components
+            - Components make the UI logic easier to organize and build
+            - Examples of possible components:
+                - Problem View/Display
+                - Information Panel
+                - etc.
+*/
+
+// eslint-disable-next-line no-console
+console.log('yes');
