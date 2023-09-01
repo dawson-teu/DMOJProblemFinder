48ff8aaa1b719d4367fc50f43ce75676402c4f12
Dawson Teu <dawsonteu@gmail.com>
2023-09-01 02:43:11 -0400

    Add Starter Code

--- src/index.js
+++ src/index.js
@@ -36,7 +36,61 @@
                 - Problem View/Display
                 - Information Panel
                 - etc.
-*/
 
-// eslint-disable-next-line no-console
-console.log('yes');
+    Refactoring
+        - Add error handling to network requests
+            - Think about alternatives to try/catch but use if necessary
+        - Add a server to act as a CORS proxy
+            - It should take in DMOJ request and send results with CORS headers
+        - Add bundling with webpack to allow babel to work
+            - Look at TetrisClone for configuration details
+        
+*/
+// This code is meant as a base/starting point for the rest of this project
+
+const baseURL = 'https://cors-anywhere.herokuapp.com/https://dmoj.ca/api';
+const defaultOptions = { headers: { origin: 'http://localhost:8080' } };
+
+// Followed Users - Display problems from followed users
+// Given an array of users and n problems to display, this function displays the last n problems the user submitted
+const followedUsersProblems = async (users, numProblems) => {
+    const problemsListData = await fetch(`${baseURL}/problem/list`, defaultOptions);
+    const problemsList = await problemsListData.json();
+
+    const requests = [];
+    for (let i = 0; i < users.length; i += 1) {
+        requests.push(fetch(`${baseURL}/user/submissions/${users[i]}`, defaultOptions));
+    }
+    const responses = await Promise.all(requests);
+
+    const jsonRequests = [];
+    for (let i = 0; i < responses.length; i += 1) {
+        jsonRequests.push(responses[i].json());
+    }
+    const submissions = await Promise.all(jsonRequests);
+
+    for (let i = 0; i < submissions.length; i += 1) {
+        // eslint-disable-next-line no-console
+        console.log(users[i]);
+
+        const userSubmissions = Object.values(submissions[i]);
+        const userProblems = [];
+
+        let index = 0;
+        while (userProblems.length < numProblems) {
+            const userSubmission = userSubmissions[userSubmissions.length - index - 1];
+            const problemName = problemsList[userSubmission.problem].name;
+            if (!userProblems.includes(problemName)) {
+                userProblems.push(problemName);
+            }
+            index += 1;
+        }
+
+        for (const name of userProblems) {
+            // eslint-disable-next-line no-console
+            console.log(name);
+        }
+    }
+};
+
+followedUsersProblems(['dnteu', 'maillew', 'jdabtieu', 'charliezhao06'], 10);
